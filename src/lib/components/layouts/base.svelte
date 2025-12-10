46171748c82c5d020621e27572ad31616871b0f1
Simon Huang <huangyuzhang@88.com>
2025-12-10 16:40:22 +0800

    add layouts/base.svelte and use it in (landing-page)/+layout.svelte

---
+++ src/lib/components/layouts/base.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { SquareLines } from "./background/square-lines";
+	import Header from "./header.svelte";
+	import Footer from "./footer.svelte";
+    let { children } = $props();
+</script>
+
+<SquareLines class=""/>
+<Header />
+<div class="max-w-5xl min-h-screen mx-auto">
+    {@render children?.()}
+</div>
+<Footer />
