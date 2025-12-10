e009874201df0aeb5439489bdf966a921ca649a5
Simon Huang <huangyuzhang@88.com>
2025-12-10 16:27:56 +0800

    add config.ts for global variables

---
+++ src/lib/config.ts
@@ -0,0 +1,46 @@
+const site = {
+    title: 'Svelte Starter',
+    description: 'A starter template built with SvelteKit, Tailwind CSS and more',
+    url: 'https://github.com/huangyuzhang/svelte-starter',
+    author: 'Simon Huang',
+}
+const nav = {
+    main: [
+        {
+            "name": "Home",
+            "url": "/"
+        },
+        {
+            "name": "Posts",
+            "url": "/posts"
+        },
+        {
+            "name": "Projects",
+            "url": "/projects"
+        },
+        {
+            "name": "About",
+            "url": "/about"
+        }
+    ],
+    footer: [
+        {
+            "name": "Home",
+            "url": "/"
+        },
+        {
+            "name": "Posts",
+            "url": "/posts"
+        },
+        {
+            "name": "Projects",
+            "url": "/projects"
+        },
+        {
+            "name": "About",
+            "url": "/about"
+        }
+    ]
+}
+
+export { site, nav };
