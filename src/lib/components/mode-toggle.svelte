d4633bc2fc2c1297ea4bcfd9cbb7503e38606d27
Simon Huang <huangyuzhang@88.com>
2025-12-10 14:23:37 +0800

    add component mode-toggle.svelte

---
+++ src/lib/components/mode-toggle.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { toggleMode } from "mode-watcher";
+    import { Button } from "$lib/components/ui/button/index.js";
+    import { IconMoon, IconSun } from "@tabler/icons-svelte";
+</script>
+ 
+<Button onclick={toggleMode} variant="outline" size="icon" class="cursor-pointer">
+    <IconSun 
+        class="transition-all! size-[1.2rem] rotate-0 scale-100 dark:-rotate-90 dark:scale-0"
+    />
+    <IconMoon 
+        class="transition-all! absolute size-[1.2rem] rotate-90 scale-0 dark:rotate-0 dark:scale-100"
+    />
+    <span class="sr-only">Toggle theme</span>
+</Button>
