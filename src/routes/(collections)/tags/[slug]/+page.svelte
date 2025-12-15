b62c9e5fb488e1115e23da865ea9b85f6feff0e8
Simon Huang <huangyuzhang@88.com>
2025-12-15 15:04:16 +0800

    fix single tag page layout error

--- src/routes/(collections)/tags/[slug]/+page.svelte
+++ src/routes/(collections)/tags/[slug]/+page.svelte
@@ -2,16 +2,12 @@
 	import { Button } from '$lib/components/ui/button';
 	import { site } from '$lib/config/index';
 	import PostListItem from '$lib/components/collections/post-list-item.svelte';
-	import ProductListItem from '$lib/components/collections/product-list-item.svelte'; // 确保导入
 	import { m } from '$lib/paraglide/messages';
 	import { getLocale } from '$lib/paraglide/runtime';
+	import ProductCard from '$lib/components/collections/product-card.svelte';
 
-	// 使用 +page.ts 中导出的类型
 	let { data } = $props();
-
 	const locale = getLocale();
-
-	// 简化后的内容配置
 	const content = {
 		en: {
 			title: 'Tag',
@@ -31,7 +27,6 @@
 		}
 	};
 
-	// Rune：计算所有内容的数量
 	const totalItems = $derived(data.posts.length + data.products.length);
 </script>
 
@@ -54,6 +49,18 @@
 </section>
 
 <section class="relative overflow-hidden px-4 my-8 sm:px-6 lg:px-8 space-y-12">
+	{#if data.products.length > 0}
+		<div>
+			<h2 class="text-2xl font-semibold mb-6 border-b pb-2">
+				{content[locale].product_section} ({data.products.length})
+			</h2>
+			<div class="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+				{#each data.products as product}
+					<ProductCard {product} />
+				{/each}
+			</div>
+		</div>
+	{/if}
 	{#if data.posts.length > 0}
 		<div>
 			<h2 class="text-2xl font-semibold mb-6 border-b pb-2">
@@ -62,19 +69,6 @@
 			<div class="flex flex-col divide-y divide-border">
 				{#each data.posts as post}
 					<PostListItem {post} />
-				{/each}
-			</div>
-		</div>
-	{/if}
-
-	{#if data.products.length > 0}
-		<div>
-			<h2 class="text-2xl font-semibold mb-6 border-b pb-2">
-				{content[locale].product_section} ({data.products.length})
-			</h2>
-			<div class="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-				{#each data.products as product}
-					<ProductListItem {product} />
 				{/each}
 			</div>
 		</div>
